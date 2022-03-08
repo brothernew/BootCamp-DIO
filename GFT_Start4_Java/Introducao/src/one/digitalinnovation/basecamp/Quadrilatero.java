dc7e07f76e2dbd668d7a9145ebfdec8db08089d2
Thiago Pedrozo <thirp.impacta@gmail.com>
2022-03-08 09:24:37 -0300

    Update

---
+++ GFT_Start4_Java/Introducao/src/one/digitalinnovation/basecamp/Quadrilatero.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.basecamp;
+
+public class Quadrilatero {
+
+    public static void area(double lado){
+
+        System.out.println("Área do quadrado: " + "\n" + lado * lado + "\n");
+    }
+
+    public static void area(double lado1, double lado2){
+
+        System.out.println("Área do retângulo: " + "\n" + lado1 * lado2 + "\n");
+    }
+
+    public static void area(double baseMaior, double baseMenor, double altura){
+
+        System.out.println("Área do trapézio: " + "\n" + ((baseMaior+baseMenor)*altura) / 2 + "\n");
+    }
+    
+}
