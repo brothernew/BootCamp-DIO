710e85520ab503e438609c61cc1ec44f03a4f736
Thiago Pedrozo <thirp.impacta@gmail.com>
2022-03-09 12:55:09 -0300

    Update

--- GFT_Start4_Java/Repeticao_Arrays/src/br/com/dio/exercicios/loops/Ex2_Nota.java
+++ GFT_Start4_Java/Repeticao_Arrays/src/br/com/dio/exercicios/loops/Ex2_Nota.java
@@ -1,9 +1,6 @@
 package br.com.dio.exercicios.loops;
-
 import java.util.Scanner;
-
 public class Ex2_Nota {
-
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -11,16 +8,13 @@ public class Ex2_Nota {
         int nota;
 
         while(true){
-
             System.out.println("Informe nota: ");
             nota = sc.nextInt();
-            if(nota > 0 && nota <= 10){
+            if(nota >= 0 && nota <= 10){
                 break;
             }else{
                 System.out.println("A nota "+nota+" informada não é válida, informe outra nota: ");
-
             }                        
         }System.out.println("Nota "+nota+" válida!!!");
-    }
-    
+    }    
 }
