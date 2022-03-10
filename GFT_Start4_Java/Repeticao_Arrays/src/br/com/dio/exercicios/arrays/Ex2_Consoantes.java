744ebf7075a049719fa631cdd6658014eaae9666
Thiago Pedrozo <thirp.impacta@gmail.com>
2022-03-10 20:54:40 -0300

    Update

--- GFT_Start4_Java/Repeticao_Arrays/src/br/com/dio/exercicios/arrays/Ex2_Consoantes.java
+++ GFT_Start4_Java/Repeticao_Arrays/src/br/com/dio/exercicios/arrays/Ex2_Consoantes.java
@@ -17,20 +17,22 @@ public class Ex2_Consoantes {
             String letra = sc.next();
 
             if(!(letra.equalsIgnoreCase("a")|
-                letra.equalsIgnoreCase("e")|
-                letra.equalsIgnoreCase("i")|
-                letra.equalsIgnoreCase("o")|
-                letra.equalsIgnoreCase("u"))){
-                cons[count] = letra;
-                qtdCons++;
+                 letra.equalsIgnoreCase("e")|
+                 letra.equalsIgnoreCase("i")|
+                 letra.equalsIgnoreCase("o")|
+                 letra.equalsIgnoreCase("u"))){
+                 cons[count] = letra;
+                 qtdCons++;
+                }
+                count++;
+            } while (count < cons.length);
+            
+            System.out.println("\nConsoantes: ");
+            
+            for (String consoante : cons) {
+                if(consoante != null)
+                System.out.print(consoante + " ");
             }
-            count++;            
-        } while (count < cons.length);
-
-        System.out.println("Consoantes: ");
-        for (String consoante : cons) {
-            if(consoante != null)
-            System.out.print(consoante + " ");
+            System.out.println("\nQuantidade de consoantes: "+qtdCons);
         }
-    }    
-}
+    }
